749bb852dba92c7981569a96c20a73874add62cd
Ricardo Sturk <37121220+rsturk87@users.noreply.github.com>
2020-07-13 16:52:58 -0300

    Responsive for tablets added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function randomBg() {
     const bg = ['https://images.unsplash.com/photo-1428908728789-d2de25dbd4e2',
-                    'https://images.unsplash.com/photo-1490131784822-b4626a8ec96a',
-                    'https://images.unsplash.com/photo-1476108621677-3c620901b5e7',
-                    'https://images.unsplash.com/photo-1535270969328-27b968457185'];
+                'https://images.unsplash.com/photo-1490131784822-b4626a8ec96a',
+                'https://images.unsplash.com/photo-1476108621677-3c620901b5e7',
+                'https://images.unsplash.com/photo-1535270969328-27b968457185'];
 
     $('body').css({
         'background' : 'url('+ bg[Math.floor(Math.random() * bg.length)] + ') no-repeat',
